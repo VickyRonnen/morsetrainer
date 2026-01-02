16e18c180186f1fc8d73f9a1519b8d0f600458a6
Vicky Ronnen <vicky.ronnen@upcmail.nl>
2026-01-02 11:54:58 +0100

    remove tests

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-
-import {v4 as uuidv4} from 'uuid';
+import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 
 @Component({
@@ -11,34 +9,5 @@ import {RouterLink} from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home implements OnInit {
-  protected localStorageWorking: boolean = false;
-  protected audioContextWorking: boolean = AudioContext !== undefined;
-  protected speechSynthesisWorking: boolean = false;
-
-  ngOnInit(): void {
-    this.testLocalStorage();
-    this.testSpeechSynthesis();
-    this.testAudioContext();
-  }
-
-  protected testAudioContext() {
-    const ctx = new AudioContext();
-    this.audioContextWorking = ctx !== undefined;
-    return this.audioContextWorking;
-  }
-
-  protected testSpeechSynthesis() {
-    const tts = new SpeechSynthesisUtterance();
-    this.speechSynthesisWorking = tts != undefined;
-    return this.speechSynthesisWorking;
-  }
-
-  private testLocalStorage() {
-    const uuid = uuidv4();
-    localStorage.setItem(uuid, uuid);
-    const result = localStorage.getItem(uuid);
-    localStorage.removeItem(uuid);
-    this.localStorageWorking = uuid === result;
-  }
+export class Home {
 }
