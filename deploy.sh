ba4f62ba58828ad7feb26ec4f3d6a8c04108d8c7
Vicky Ronnen <vicky.ronnen@upcmail.nl>
2026-08-07 16:38:04 +0200

    pa5wpm.nl deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+DOMAIN=pa5wpm.nl
 ng build -c production|| exit 1
-ssh root@pa5wpm.nl rm -rf /var/www/vhosts/pa5wpm.nl/httpdocs/*
-ssh root@pa5wpm.nl mkdir -p /var/www/vhosts/pa5wpm.nl/httpdocs
-scp -r dist/morsetrainer/browser/* root@pa5wpm.nl:/var/www/vhosts/pa5wpm.nl/httpdocs
-scp .htaccess root@pa5wpm.nl:/var/www/vhosts/pa5wpm.nl/httpdocs
+ssh root@pa5wpm.nl rm -rf /var/www/$DOMAIN
+ssh root@pa5wpm.nl mkdir -p /var/www/$DOMAIN
+scp -r dist/morsetrainer/browser/* root@pa5wpm.nl:/var/www/$DOMAIN
+#scp .htaccess root@pa5wpm.nl:/var/www/vhosts/$DOMAIN
