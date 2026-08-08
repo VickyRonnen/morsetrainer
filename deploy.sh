c4d3733b2d6fb1459ce478bd2d286672eaf8da0a
Vicky Ronnen <vicky.ronnen@upcmail.nl>
2026-08-08 22:33:29 +0200

    pa5wpm.nl deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DOMAIN=pa5wpm.nl
-ng build -c production|| exit 1
+npm run build -- -c production || exit 1
 ssh root@pa5wpm.nl rm -rf /var/www/$DOMAIN
 ssh root@pa5wpm.nl mkdir -p /var/www/$DOMAIN
 scp -r dist/morsetrainer/browser/* root@pa5wpm.nl:/var/www/$DOMAIN
-#scp .htaccess root@pa5wpm.nl:/var/www/vhosts/$DOMAIN
+scp strato/100-pa5wpm.nl.conf root@pa5wpm.nl:/etc/apache2/sites-available
+ssh root@pa5wpm.nl a2ensite 100-pa5wpm.nl.conf
+ssh root@pa5wpm.nl systemctl reload apache2
--- localdeploy.sh
+++ localdeploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ng build -c production|| exit 1
+set -x
+npm run build -- -c production|| exit 1
 sudo rm -rf /var/www/pa5wpm
 sudo mkdir -p /var/www/pa5wpm
 sudo cp -r dist/morsetrainer/browser/* /var/www/pa5wpm
-sudo cp .htaccess /var/www/pa5wpm
 sudo systemctl daemon-reload
 sudo systemctl reload httpd
+sudo ls -al /var/www/pa5wpm
