fddb6401add11ad832975344639611c8138c343e
Vicky Ronnen <vicky.ronnen@upcmail.nl>
2026-08-14 12:31:06 +0200

    deployment script updates

--- localdeploy.sh
+++ localdeploy.sh
@@ -4,9 +4,8 @@ npm run build -- -c production|| exit 1
 sudo rm -rf /var/www/pa5wpm
 sudo mkdir -p /var/www/pa5wpm
 sudo cp -r dist/morsetrainer/browser/* /var/www/pa5wpm
-sudo cp local/pa5wpm.denkzelf.nl.conf /etc/httpd/conf.d/
-sudo chown root:root /etc/httpd/conf.d/pa5wpm.denkzelf.nl.conf
-sudo systemctl daemon-reload
+sudo cp local/pa5wpm.pc.test.conf /etc/httpd/conf.d/
+sudo chown root:root /etc/httpd/conf.d/pa5wpm.pc.test.conf
 sudo systemctl reload httpd
 sudo ls -al /var/www/pa5wpm
-sudo ls -al /etc/httpd/conf.d/pa5wpm.denkzelf.nl.conf
+sudo ls -al /etc/httpd/conf.d/pa5wpm.pc.test.conf
